b0642d2ac8c1d3fe115ea5b74b77cc3784561bb7
Javlonbek fayzullaev <fayzullaev.javlonbek.d@gmail.com>
2023-04-03 12:27:17 +0500

    Add project files.

---
+++ ConsoleApp/MockData/Data.cs
@@ -0,0 +1,20 @@
+﻿using ConsoleApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.MockData
+{
+    public class Data
+    {
+        public static List<User> Users { get; set; } = new List<User>()
+        {
+            new User() { Id = 1, Name = "Muhammad", Email = "muhammad@gmail.com", Password = "123456"},
+            new User() { Id = 2, Name = "Umar", Email = "umar@gmail.com", Password = "123456"},
+            new User() { Id = 3, Name = "Kholid", Email = "kholid@gmail.com", Password = "123456"},
+            new User() { Id = 4, Name = "Valid", Email = "valid@gmail.com", Password = "123456"}
+        };
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp.Interfaces;
+using ConsoleApp.Models;
+using ConsoleApp.Security;
+using ConsoleApp.Services;
+using System.Net;
+
+class Program
+{
+    public static void Main()
+    {
+       Crypt crypt = new Crypt();
+        var str = crypt.EnCrypt("Rizq");
+        Console.WriteLine(str);
+        Console.WriteLine(crypt.DeCrypt(str));
+    }
+   
+}
